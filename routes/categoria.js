6fc80cffe3a8678a3c7c5f96f535c87b2ce2abd8
gumayago <guma.yago@gmail.com>
2026-03-31 17:27:17 -0300

    adaptando categoria
    adaptando rota categoria

--- routes/categoria.js
+++ routes/categoria.js
@@ -3,3 +3,20 @@
 app.get('/categoria', (req, res) => {
   res.send('<h1> Categorias: </h1> ')
 });
+
+
+// SELECT * FROM piloto;
+app.get('/categoria', async (req, res) => {
+    const dados = await db.query( "SELECT * FROM piloto")
+    console.log(dados, )
+    res.json(dados[0])
+})
+
+//  criar rota que busque todos as categorias
+// add um planeta no bd
+app.get('/planeta', async (req, res) => {
+    const [dados] = await db.query("SELECT * FROM planeta")
+    res.json(dados)
+    // aqui transferimos o indice "[0]" do objeto JSON para a VARIAVEL "dados" para otimizar e vc ""SEMPRE"" vai querer o indicde O que vem do banco, logo os dados da entidade q vc deseja.
+})
+
