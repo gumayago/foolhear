0efe6642b767f44e657683df21bef536250d8070
gumayago <guma.yago@gmail.com>
2026-03-31 17:27:02 -0300

    concerto index.js
    concerto

--- index.js
+++ index.js
@@ -2,14 +2,26 @@
 // Para o trabalho dividir os server's controller's, e as rotas. (para distrinchar corretamente)
 // COMANDOS: npm init -y, npm install express --save, node + nome(index.js) e node --watch index.js
 
-const express = require('express');
-const app = express();
-const port = 3000;
+// requisitando a biblioteca do express (API) 
+const express = require('express')
+// chamando a biblioteca que instalamos do mysql
+const mysql = require("mysql2/promisse")
+const app = express()
+const port = 3000
+// Express USE objetos (json) 
+app.use(express.json())
 
+// chamando o banco de dados  
+const db = mysql.createPool({
+    host: "localhost",
+    user:"hoot",
+    password: "senacrs",
+    database:"bd"
+})
 
-
-
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 
 
